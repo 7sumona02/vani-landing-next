3fcf594e173b6a6c84d3bb29b6105df5a63b7b22
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-07-01 18:20:45 +0530

    choose section

--- components/Choose1.tsx
+++ components/Choose1.tsx
@@ -6,18 +6,48 @@ const Choose1 = () => {
            <div className='pb-6 leading-[5rem] text-6xl tracking-wide font-semibold md:leading-[6.5rem] bg-clip-text text-transparent bg-gradient-to-br from-white to-gray-500'>Why Creators Choose Vaani</div>
            <div className="grid grid-cols-3 gap-6">
                 <div className="">
-                    <div className="h-60 rounded-2xl bg-neutral-800 p-10">
-                        <div>
-                            <div className="text-3xl font-medium text-[#e9e9e9]">Sumona</div>
-                            <div className="text-xl font-medium text-white/60">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, accusamus?</div>
+                    <div className="h-[20rem] w-full rounded-2xl bg-gradient-to-bl from-neutral-900 to-neutral-800  border border-white/20 p-10">
+                        <div className="space-y-3">
+                            <div className="text-3xl font-medium text-[#e9e9e9]">Instant download</div>
+                            <div className="text-lg font-medium text-white/60">WAV, MP3, OGG, 24 kHz—ready for post-production or direct publish.</div>
                         </div>
                         <div>
                             <img src='' alt='' />
                         </div>
                     </div>
                 </div>
-                <div className="cols-span-2">
-
+                <div className="col-span-2">
+                    <div className="h-[20rem] w-full rounded-2xl bg-gradient-to-bl from-neutral-900 to-neutral-800  border border-white/20 p-10">
+                        <div className="space-y-3">
+                            <div className="text-3xl font-medium text-[#e9e9e9]">Instant download</div>
+                            <div className="text-lg font-medium text-white/60">WAV, MP3, OGG, 24 kHz—ready for post-production or direct publish.</div>
+                        </div>
+                        <div>
+                            <img src='' alt='' />
+                        </div>
+                    </div>
+                </div>
+                <div className="col-span-2">
+                    <div className="h-[20rem] w-full rounded-2xl bg-gradient-to-bl from-neutral-900 to-neutral-800  border border-white/20 p-10">
+                        <div className="space-y-3">
+                            <div className="text-3xl font-medium text-[#e9e9e9]">Instant download</div>
+                            <div className="text-lg font-medium text-white/60">WAV, MP3, OGG, 24 kHz—ready for post-production or direct publish.</div>
+                        </div>
+                        <div>
+                            <img src='' alt='' />
+                        </div>
+                    </div>
+                </div>
+                <div className="">
+                    <div className="h-[20rem] w-full rounded-2xl bg-gradient-to-bl from-neutral-900 to-neutral-800  border border-white/20 p-10">
+                        <div className="space-y-3">
+                            <div className="text-3xl font-medium text-[#e9e9e9]">Instant download</div>
+                            <div className="text-lg font-medium text-white/60">WAV, MP3, OGG, 24 kHz—ready for post-production or direct publish.</div>
+                        </div>
+                        <div>
+                            <img src='' alt='' />
+                        </div>
+                    </div>
                 </div>
            </div>
         </div>
